d3dd1dc972baf148d9e2a08dace34f39019fa0ad
hany18h <hanyshrabia818@gmail.com>
2025-10-20 01:04:44 +0300

    Update Hijalacom.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/Hijalacom.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/Hijalacom.kt
@@ -5,26 +5,9 @@ import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
 
-/**
- * Parser لموقع Hijala.com
- * الموقع يستخدم WordPress (MangaReader theme)
- * 
- * ملاحظة مهمة: الموقع يستخدم WordPress وليس Blogger!
- * تأكد من أن المصدر مسجل بشكل صحيح
- */
 @MangaSourceParser("HIJALACOM", "Hijala", "ar")
 internal class Hijalacom(context: MangaLoaderContext) :
 	MangaReaderParser(context, MangaParserSource.HIJALACOM, "hijala.com", pageSize = 20, searchPageSize = 10) {
 	
-	// الموقع يستخدم WordPress مع theme MangaReader
 	override val listUrl = "/manga"
-	
-	// نمط التاريخ المستخدم في الموقع
-	override val datePattern = "MMMM dd, yyyy"
-	
-	// تفعيل حماية الموقع إذا كانت موجودة
-	override val isNetShieldProtected = false
-	
-	// الموقع لا يستخدم ترميز base64 للصور
-	override val encodedSrc = false
 }
