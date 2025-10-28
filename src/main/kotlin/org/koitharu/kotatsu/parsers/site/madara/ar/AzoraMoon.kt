005bcb8342b2b885e7a1b7c56809f4ed84d48b64
hany18h <hanyshrabia818@gmail.com>
2025-10-28 15:26:45 +0300

    Update AzoraMoon.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AzoraMoon.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AzoraMoon.kt
@@ -2,13 +2,18 @@ package org.koitharu.kotatsu.parsers.site.madara.ar
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.model.Manga
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("AZORAMOON", "AzoraMoon", "ar")
 internal class AzoraMoon(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.AZORAMOON, "azoramoon.com", pageSize = 10) {
+	
 	override val tagPrefix = "series-genre/"
 	override val listUrl = "series/"
-	override val selectRelated = "" // تعطيل البحث عن المانجا المشابهة
+	
+	override suspend fun getRelatedManga(seed: Manga): List<Manga> {
+		return emptyList()
+	}
 }
