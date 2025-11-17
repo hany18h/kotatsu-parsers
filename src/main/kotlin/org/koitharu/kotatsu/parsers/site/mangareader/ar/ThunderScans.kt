d96133ff1dce38cd51470c499ac6507c603e09f2
hany18h <hanyshrabia818@gmail.com>
2025-11-18 01:19:00 +0200

    Update ThunderScans.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/ThunderScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/ThunderScans.kt
@@ -1,23 +1,24 @@
-package org.koitharu.kotatsu.parsers.site.mangareader.ar
-
-import org.koitharu.kotatsu.parsers.MangaLoaderContext
-import org.koitharu.kotatsu.parsers.MangaSourceParser
-import org.koitharu.kotatsu.parsers.model.MangaListFilterCapabilities
-import org.koitharu.kotatsu.parsers.model.MangaParserSource
-import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
-
 @MangaSourceParser("THUNDERSCANS", "ThunderScans", "ar")
 internal class ThunderScans(context: MangaLoaderContext) :
-	MangaReaderParser(
-		context,
-		MangaParserSource.THUNDERSCANS,
-		"lavascans.com",
-		pageSize = 32,
-		searchPageSize = 10,
-	) {
-	override val filterCapabilities: MangaListFilterCapabilities
-		get() = super.filterCapabilities.copy(
-			isTagsExclusionSupported = false,
-		)
-	override val selectChapter = ".eplister > ul > li"
+    MangaReaderParser(
+        context,
+        MangaParserSource.THUNDERSCANS,
+        "lavascans.com",
+        pageSize = 32,
+        searchPageSize = 10,
+    ) {
+
+    // لازم علشان تعدّي Cloudflare
+    override val isNetShieldProtected = true
+
+    // المسار الأساسي في lavatoons/lavascans
+    override val listUrl = "/manga"
+
+    // مواقع الوردبريس بدأت تغيّر الهيكل
+    override val selectChapter =
+        "div.eplister ul li, #chapterlist ul li"
+
+    // دعم شكل ts_reader القديم والجديد
+    override val selectTestScript =
+        "script#ts-reader, script:contains(ts_reader)"
 }
