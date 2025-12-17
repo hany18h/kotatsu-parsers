91a05890eee1a38015211f0d4fefca7dfb8ea192
hany18h <hanyshrabia818@gmail.com>
2025-12-17 03:44:32 +0200

    Update ThunderScans.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/ThunderScans.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/mangareader/ar/ThunderScans.kt
@@ -1,10 +1,14 @@
 package org.koitharu.kotatsu.parsers.site.mangareader.ar
 
+import org.json.JSONObject
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
+import org.koitharu.kotatsu.parsers.model.MangaChapter
+import org.koitharu.kotatsu.parsers.model.MangaPage
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.mangareader.MangaReaderParser
+import org.koitharu.kotatsu.parsers.util.*
 
 @MangaSourceParser("LAVATOONS", "Lavatoons", "ar", ContentType.MANGA)
 internal class Lavatoons(context: MangaLoaderContext) :
@@ -16,12 +20,65 @@ internal class Lavatoons(context: MangaLoaderContext) :
         searchPageSize = 10,
     ) {
     
-    override val isNetShieldProtected = false
+    override val isNetShieldProtected = true
     
     override val selectChapter = "div.eplister#chapterlist li"
     
-    override val selectTestScript = 
-        "script:contains(ts_reader.run), script#ts-reader, script:contains(ts_reader)"
-    
-    override val selectPage = "div#readerarea img, div.reading-content img"
+    override suspend fun getPages(chapter: MangaChapter): List<MangaPage> {
+        val chapterUrl = chapter.url.toAbsoluteUrl(domain)
+        val doc = webClient.httpGet(chapterUrl).parseHtml()
+        
+        // البحث عن script الذي يحتوي على ts_reader.run
+        val scripts = doc.select("script")
+        
+        for (script in scripts) {
+            val scriptContent = script.html()
+            
+            if (scriptContent.contains("ts_reader.run")) {
+                try {
+                    // استخراج JSON من ts_reader.run({...})
+                    val startIdx = scriptContent.indexOf("ts_reader.run(") + 14
+                    val endIdx = scriptContent.indexOf("});", startIdx)
+                    
+                    if (startIdx > 14 && endIdx > startIdx) {
+                        val jsonText = scriptContent.substring(startIdx, endIdx + 1)
+                        val json = JSONObject(jsonText)
+                        val sources = json.getJSONArray("sources")
+                        
+                        if (sources.length() > 0) {
+                            val images = sources.getJSONObject(0).getJSONArray("images")
+                            val pages = ArrayList<MangaPage>(images.length())
+                            
+                            for (i in 0 until images.length()) {
+                                val url = images.getString(i)
+                                pages.add(
+                                    MangaPage(
+                                        id = generateUid(url),
+                                        url = url,
+                                        preview = null,
+                                        source = source,
+                                    )
+                                )
+                            }
+                            
+                            return pages
+                        }
+                    }
+                } catch (e: Exception) {
+                    // إذا فشل parsing، نحاول الطريقة القديمة
+                }
+            }
+        }
+        
+        // fallback: الطريقة القديمة
+        return doc.select("div#readerarea img").map { img ->
+            val url = img.src() ?: img.attr("data-src")
+            MangaPage(
+                id = generateUid(url),
+                url = url,
+                preview = null,
+                source = source,
+            )
+        }
+    }
 }
