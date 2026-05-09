294005660557c5d859a24c99949a0d2f75c275d1
Hema <76667031+hema995@users.noreply.github.com>
2026-05-09 23:18:15 +0300

    Update AnyoneManga.kt

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AnyoneManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/ar/AnyoneManga.kt
@@ -6,5 +6,5 @@ import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("ANYONEMANGA", "AnyoneManga", "ar")
-internal class Eshadow(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.ESHADOW, "anyonemanga.com", pageSize = 20)
+internal class AnyoneManga(context: MangaLoaderContext) :
+	MadaraParser(context, MangaParserSource.ANYONEMANGA, "anyonemanga.com", pageSize = 20)
